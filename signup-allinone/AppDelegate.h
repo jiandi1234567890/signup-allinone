df1f9e75d2cb89cd0ed77d8f9591eb26476dc096
jiandi1234567890 <550392461@qq,com>
2016-05-12 14:22:41 +0800

    Initial Commit

---
+++ signup-allinone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  signup-allinone
+//
+//  Created by 张海禄 on 16/5/12.
+//  Copyright © 2016年 张海禄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ signup-allinone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  signup-allinone
+//
+//  Created by 张海禄 on 16/5/12.
+//  Copyright © 2016年 张海禄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
